b28cbf15e798b2c05aaeeaffb461bac6bb9a1fe4
Christian Onyeukwu <christianonyeukwu@gmail.com>
2018-08-21 17:11:36 +0100

    script update

---
+++ tradeTypes_skill.php
@@ -0,0 +1,10 @@
+<?php
+   
+   header("Access-Control-Allow-Origin:*");
+   header("Content-Type:application/json; charset=UTF-8");
+   require_once("conn.settings.php");
+   require_once("core.php");
+   
+   $log_data="";
+   
+   ?>
